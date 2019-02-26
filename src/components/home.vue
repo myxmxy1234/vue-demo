19d1054bb4d09c80de88ee3ee6e3e4f1542816c0
马煜鑫 <ex-mayuxin001@pingan.com.cn>
2019-02-26 09:42:57 +0800

    haha

---
+++ src/components/home.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class='home'>
+        <button @click="add">{{num}}</button>
+        <a :href="url">点击就送</a>
+        <!-- <router-link to='/hello' tag="a">go helloworld page</router-link> -->
+        <!-- <router-view></router-view> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data() {
+        return {
+            num: 0,
+            url: 'https://www.baidu.com'
+        }
+    },
+    methods: {
+        add() {
+            this.num++
+        }
+    },
+    watch: {
+        num(newVal, oldVal) {
+            console.log(newVal, oldVal)
+        }
+    },
+    beforeCreate () {
+        console.log('beforeCreate')
+    },
+    created () {
+        console.log('created')
+    },
+    beforeMount () {
+        console.log('beforeMount')
+    },
+    mounted () {
+        console.log('mounted')
+    },
+    beforeUpdate () {
+        // console.log('beforeUpdate')
+    },
+    updated () {
+        // console.log('updated')
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
